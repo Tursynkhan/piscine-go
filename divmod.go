a51b21bb9958da69d2203526bbf001831cf1f439
tursynkhan <you@example.com>
2022-04-22 05:38:19 +0600

    divmod

--- divmod.go
+++ divmod.go
@@ -1,7 +1,13 @@
 package piscine
 
-func DivMod(a int,b int,div*int, mod *int){
-	*div := a/b
-	*mod := a%b
+import "fmt"
 
+func DivMod(a int, b int, div *int, mod *int) {
+	a := 13
+	b := 2
+	div := a / b
+	mod := a % b
+	piscine.DivMod(a, b, &div, &mod)
+	fmt.PrintLn(div)
+	fmt.PrintLn(mod)
 }
