51765d6493c1929c3135bd95e3d5ae96079c3897
tursynkhan <tursynkhan001@gmail.com>
2022-04-22 00:52:37 +0600

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for z := '0'; z <= '9'; z++ {
+				for d := '0'; d <= '9'; d++ {
+					if j < d {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(z)
+						z01.PrintRune(d)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
+}
