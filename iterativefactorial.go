585547fd0e35583ffc173172ced5e8bbb9856af3
tursynkhan <tursynkhan0012gmail.com>
2022-04-26 00:14:10 +0600

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,16 +2,17 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	factorial := 1
-	if nb < 0 || nb > 39 {
-		return 0
+	if nb == 0 {
+		return 1
 	} else if nb == 0 {
 		return 1
-	}
-	for i := 1; i <= nb; i++ {
-		if i < 0 || i > 12 {
-			return 0
-		} else {
-			factorial *= i
+	} else {
+		for i := 1; i <= nb; i++ {
+			if i < 0 || i > 40 {
+				return 0
+			} else {
+				factorial *= i
+			}
 		}
 	}
 	return nb
