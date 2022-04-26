502c36807c7e83a0bb8a2f4263ac05357100e637
tursynkhan <tursynkhan001@gmail.com>
2022-04-27 01:08:10 +0600

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -3,8 +3,10 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
-	} else if index <= 1 {
+	} else if index == 1 {
 		return 1
+	} else if index == 0 {
+		return 0
 	} else {
 		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
