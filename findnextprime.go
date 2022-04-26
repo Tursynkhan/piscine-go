075e3a4389c07ec7ebdd3aa21d0444a3d9e5789d
tursynkhan <tursynkhan001@gmail.com>
2022-04-27 05:56:39 +0600

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,13 +1,21 @@
 package piscine
 
-func FindNextPrime(nb int) int {
-	if IsPrime(nb) {
-		return nb
-	} else {
-		n := nb + 1
-		for IsPrime(n) == false {
-			n++
+func IsPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
 		}
-		return n
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	for i := nb + 1; i < 1000000; i++ {
+		if IsPrime(i) {
+			return i
+		}
 	}
 }
