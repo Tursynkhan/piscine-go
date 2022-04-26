bbc121537743be926071b23c6b0ae1e4f692a1d0
tursynkhan <tursynkhan001@gmail.com>
2022-04-26 20:18:48 +0600

    power

---
+++ iterativepower.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func IterativePower(n int, power int) int {
+	result := 1
+	if power < 0 || power > 40 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else {
+		for i := 0; i < power; i++ {
+			result *= n
+		}
+		return result
+	}
+}
+
+func main() {
+	fmt.Println(IterativePower(4, 3))
+}
