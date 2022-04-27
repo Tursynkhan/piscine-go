67584b4cb8892634bd9fa078d4c198351b9e9676
tursynkhan <tursynkhan001@gmail.com>
2022-04-27 06:00:08 +0600

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -6,7 +6,7 @@ func IsPrime(nb int) bool {
 	}
 	prime := 1
 	for i := 2; i < nb; i++ {
-		if ((nb % i) == 0) {
+		if (nb % i) == 0 {
 			return false
 		}
 	}
@@ -17,10 +17,12 @@ func IsPrime(nb int) bool {
 	}
 }
 
-
 func FindNextPrime(nb int) int {
 	if nb <= 1 {
 		return 2
 	}
-	for i := 2; !IsPrime(i); i++
+	for i := 2; !IsPrime(i); i++ {
+		return i
+	}
+	return 0
 }
