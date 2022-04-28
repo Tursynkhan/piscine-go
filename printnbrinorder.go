188a3832afcf153f122a22fd45328b500b2cfc5b
tursynkhan <tursynkhan001@gmail.com>
2022-04-28 18:22:04 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -13,8 +13,8 @@ func PrintNbrInOrder(n int) {
 	}
 	for i := 0; i < len(arr)-1; i++ {
 		for j := 0; j < len(arr)-i-1; j++ {
-			if arr[i] > arr[i+1] {
-				arr[i], arr[i+1] = arr[i+1], arr[i]
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
