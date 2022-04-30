efa5aea10e17a410097149592524bcbc5cccd2af
tursynkhan <student@ALEM-a1da4a.alem.school>
2022-04-30 17:34:49 +0600

    printwordstable

--- printwordstables.go
+++ printwordstables.go
@@ -3,9 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-	for _, value := range a {
-		if value == " " {
-			z01.PrintRune('\n')
+	for i := 0; i < len(a); i++ {
+		for index, value := range a[i] {
+			if index >= 0 {
+				z01.PrintRune(value)
+			}
 		}
+		z01.PrintRune('\n')
 	}
 }
