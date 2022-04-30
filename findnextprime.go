b595c410af0aa7b82e3f243fea431d1266362586
tursynkhan <student@ALEM-a1da4a.alem.school>
2022-04-30 19:18:38 +0600

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,25 +1,8 @@
 package piscine
 
-func IsPrime1(nb int) bool {
-	if nb == 1 || nb <= 0 {
-		return false
-	}
-	prime := 1
-	for i := 2; i < nb; i++ {
-		if (nb % i) == 0 {
-			return false
-		}
-	}
-	if prime == 1 {
-		return true
-	} else {
-		return false
-	}
-}
-
 func FindNextPrime(nb int) int {
 	for i := nb; ; i++ {
-		if IsPrime1(i) {
+		if IsPrime(i) {
 			return i
 		}
 	}
