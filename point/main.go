3eea4a73182cc42940561ec73979e66cf474ce6c
tursynkhan <student@ALEM-a1da42.alem.school>
2022-05-02 19:03:29 +0600

    point

--- point/main.go
+++ point/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
 
 type point struct {
@@ -19,5 +19,32 @@ func main() {
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	var runesx []rune
+	var runesy []rune
+
+	for points.x > 0 {
+		runesx = append(runesx, rune((points.x%10)+48))
+		points.x /= 10
+	}
+	for points.y > 0 {
+		runesy = append(runesy, rune((points.y%10)+48))
+		points.y /= 10
+	}
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for i := len(runesx) - 1; i >= 0; i-- {
+		z01.PrintRune(runesx[i])
+	}
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for j := len(runesy) - 1; j >= 0; j-- {
+		z01.PrintRune(runesy[j])
+	}
+	z01.PrintRune('\n')
 }
