e19359699765a1d6087ae7168b651dde0f4890a8
tursynkhan <tursynkhan>
2022-05-04 21:34:47 +0600

    countif

--- countif.go
+++ countif.go
@@ -1,9 +1,9 @@
 package piscine
 
 func CountIf(f func(string) bool, tab []string) int {
-	sum:=0
-	for i:=0;i<len(tab);i++{
-		if f(tab[i] == true{
+	sum := 0
+	for i := 0; i < len(tab); i++ {
+		if f(tab[i]) == true {
 			sum++
 		}
 	}
