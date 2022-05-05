c2bfe04dab558ab4f4a98cd113e5aacb2cb94104
tursynkhan <student@ALEM-f483e1.alem.school>
2022-05-05 11:15:17 +0600

    abort

--- abort.go
+++ abort.go
@@ -1,5 +1,18 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return (a + b + c + d + e) / 5
+	var runes []int
+	runes = append(runes, a)
+	runes = append(runes, b)
+	runes = append(runes, c)
+	runes = append(runes, d)
+	runes = append(runes, e)
+	for i := 0; i < len(runes)-1; i++ {
+		for j := 0; j < len(runes)-i-1; j++ {
+			if runes[j] > runes[j+1] {
+				runes[j], runes[j+1] = runes[j+1], runes[j]
+			}
+		}
+	}
+	return runes[2]
 }
