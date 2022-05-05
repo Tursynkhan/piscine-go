0453422ad0e5e316fbf0fc830deb47f79e862800
tursynkhan <student@ALEM-f483e1.alem.school>
2022-05-05 14:57:09 +0600

    issorted

--- issorted.go
+++ issorted.go
@@ -1,19 +1,17 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	ascend := false
+	descend := false
 	for i := 0; i < len(a)-1; i++ {
-		for j := 1; j < len(a)-1; j++ {
-			if a[0] < a[len(a)-1] {
-				if f(a[j], a[i]) <= 0 {
-					return false
-				}
-			}
-			if a[0] > a[len(a)-1] {
-				if f(a[i], a[j]) <= 0 {
-					return false
-				}
-			}
+		if f(a[i], a[i+1]) > 0 {
+			ascend = true
 		}
 	}
-	return true
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) < 0 {
+			descend = true
+		}
+	}
+	return !(ascend) || !(descend)
 }
