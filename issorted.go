027af69d2e69b1f15a309210cf6ad2c957f70f5a
tursynkhan <student@ALEM-f483e1.alem.school>
2022-05-05 14:49:33 +0600

    issorted

---
+++ issorted.go
@@ -0,0 +1,19 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; ; i++ {
+		for j := 1; j < len(a)-1; j++ {
+			if a[0] < a[len(a)-1] {
+				if f(a[j], a[i]) <= 0 {
+					return false
+				}
+			}
+			if a[0] > a[len(a)-1] {
+				if f(a[i], a[j]) <= 0 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
