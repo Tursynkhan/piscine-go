0382105950e2af9f2d08656ebf4bf3e722479e16
tursynkhan <student@ALEM-f483e1.alem.school>
2022-05-05 15:42:47 +0600

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	args := os.Args[1:]
 	str := []string(args)
 	for i := 0; i < len(str); i++ {
-		if str[i] == "01" || str[i] == "galaxy" || str[i] == "galaxy01" {
+		if str[i] == "01" || str[i] == "galaxy" || str[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 			return
 		}
