282de92dc1c16d2c8882c9b5f8f76df311d1141a
tursynkhan <tursynkhan>
2022-05-10 18:16:31 +0600

    eightqueens

--- eightqueens.go
+++ eightqueens.go
@@ -17,9 +17,9 @@ func IsSafe(qn int, rp int, pos [8]int) bool {
 func solve(nb int, pos [8]int) {
 	if nb == 8 {
 		for i := 0; i < 8; i++ {
-			z01.PrintRune(rune(pos[i] + '1'))
+			z01.PrintRune(rune(pos[i] + 49))
 		}
-		z01.PrintRune('\n')
+		z01.PrintRune(10)
 	} else {
 		for i := 0; i < 8; i++ {
 			if IsSafe(nb, i, pos) {
