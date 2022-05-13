1b31f5784c328b9b1a0acd615127f82aa8537eea
tursynkhan <student@ALEM-7d91a0.alem.school>
2022-05-13 23:22:11 +0600

    btree

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,11 +1,11 @@
 package piscine
 
-type TreeNode struct {
+type TreeNode1 struct {
 	Left, Right, Parent *TreeNode
 	Data                string
 }
 
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+func BTreeInsertData1(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
