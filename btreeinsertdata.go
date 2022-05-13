617b743789d8b4278396ab36b0be4900943d3bee
tursynkhan <student@ALEM-7d91a0.alem.school>
2022-05-13 22:42:07 +0600

    btreeinsertdata

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,9 +1,8 @@
-
 package piscine
 
 type TreeNode struct {
 	Left, Right, Parent *TreeNode
-	Data                 string
+	Data                string
 }
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
@@ -13,10 +12,10 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else if data > root.Data {
+	} else if data == root.Data {
+		root.Parent = BTreeInsertData(root.Parent, data)
+	} else {
 		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
 	}
-	return root	
+	return root
 }
